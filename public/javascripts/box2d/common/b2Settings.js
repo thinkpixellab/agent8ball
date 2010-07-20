1a95ef1b32eca71f604577cfe6e6c624a09672dd
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-07-20 03:23:30 +0000

    b2Settings are HOT!
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@54 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- public/javascripts/box2d/common/b2Settings.js
+++ public/javascripts/box2d/common/b2Settings.js
@@ -16,58 +16,51 @@
 * 3. This notice may not be removed or altered from any source distribution.
 */
 
-
-
-
-
-var b2Settings = Class.create();
-b2Settings.prototype = {
-
-
-
-  // Define your unit system here. The default system is
-  // meters-kilograms-seconds. For the tuning to work well,
-  // your dynamic objects should be bigger than a pebble and smaller
-  // than a house.
-  //static public const b2Settings.b2_lengthUnitsPerMeter = 1.0;
-
-  // Use this for pixels:
-
-  // Global tuning constants based on MKS units.
-
-  // Collision
-
-  // Dynamics
-
-  // Sleep
-
-  // assert
-
-  initialize: function() {}}
+/** @const */
+var b2Settings = {};
+/** @const @type {number} */
 b2Settings.USHRT_MAX = 0x0000ffff;
+/** @const @type {number} */
 b2Settings.b2_pi = Math.PI;
+/** @const @type {number} */
 b2Settings.b2_massUnitsPerKilogram = 1.0;
+/** @const @type {number} */
 b2Settings.b2_timeUnitsPerSecond = 1.0;
+/** @const @type {number} */
 b2Settings.b2_lengthUnitsPerMeter = 30.0;
+/** @const @type {number} */
 b2Settings.b2_maxManifoldPoints = 2;
+/** @const @type {number} */
 b2Settings.b2_maxShapesPerBody = 64;
+/** @const @type {number} */
 b2Settings.b2_maxPolyVertices = 8;
+/** @const @type {number} */
 b2Settings.b2_maxProxies = 1024;
+/** @const @type {number} */
 b2Settings.b2_maxPairs = 8 * b2Settings.b2_maxProxies;
+/** @const @type {number} */
 b2Settings.b2_linearSlop = 0.005 * b2Settings.b2_lengthUnitsPerMeter;
+/** @const @type {number} */
 b2Settings.b2_angularSlop = 2.0 / 180.0 * b2Settings.b2_pi;
+/** @const @type {number} */
 b2Settings.b2_velocityThreshold = 1.0 * b2Settings.b2_lengthUnitsPerMeter / b2Settings.b2_timeUnitsPerSecond;
+/** @const @type {number} */
 b2Settings.b2_maxLinearCorrection = 0.2 * b2Settings.b2_lengthUnitsPerMeter;
+/** @const @type {number} */
 b2Settings.b2_maxAngularCorrection = 8.0 / 180.0 * b2Settings.b2_pi;
+/** @const @type {number} */
 b2Settings.b2_contactBaumgarte = 0.2;
+/** @const @type {number} */
 b2Settings.b2_timeToSleep = 0.5 * b2Settings.b2_timeUnitsPerSecond;
+/** @const @type {number} */
 b2Settings.b2_linearSleepTolerance = 0.01 * b2Settings.b2_lengthUnitsPerMeter / b2Settings.b2_timeUnitsPerSecond;
+/** @const @type {number} */
 b2Settings.b2_angularSleepTolerance = 2.0 / 180.0 / b2Settings.b2_timeUnitsPerSecond;
-b2Settings.FLT_EPSILON = .0000001192092896;
-b2Settings.b2Assert = function(a)
-  {
-    if (!a){
-      var nullVec;
-      nullVec.x++;
-    }
-  };
+/** @const @type {number} */
+b2Settings.FLT_EPSILON = 0.0000001192092896;
+/** @const @type {function(boolean)} */
+b2Settings.b2Assert = function(a) {
+  if (!a) {
+    throw "Assert Failed!";
+  }
+};
