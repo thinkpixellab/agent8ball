edb61df423e72df1009112eb94b9def244bf81a6
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-07-20 23:44:15 +0000

    Making things a bit more real.
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@63 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

---
+++ public/javascripts/eightball/PoolTable.js
@@ -0,0 +1,212 @@
+goog.require('goog.math.Matrix');
+goog.provide('eightball.PoolTable');
+
+eightball.PoolTable = {};
+
+/**
+ @const
+ @type {number}
+ cm, regulation
+ */
+eightball.PoolTable.height = 148;
+
+/**
+ @const
+ @type {number}
+ cm, regulation
+ */
+eightball.PoolTable.width = 284;
+/**
+ @const
+ @type {number}
+ cm, regulation
+ */
+eightball.PoolTable.ballDiameter = 5.715;
+
+/**
+ @const
+ @type {number}
+ */
+eightball.PoolTable.bumperThickness = 10;
+
+eightball.PoolTable.createWorld = function() {
+
+  var worldAABB = new b2AABB();
+  worldAABB.minVertex.Set(-1000, -1000);
+  worldAABB.maxVertex.Set(1000, 1000);
+  var gravity = new b2Vec2(0, 0);
+  var doSleep = true;
+  var world = new b2World(worldAABB, gravity, doSleep);
+
+  eightball.PoolTable._createTable(world);
+  eightball.PoolTable._setupBalls(world);
+
+  return world;
+};
+
+eightball.PoolTable._setupBalls = function(world) {
+  var ballRadius = eightball.PoolTable.ballDiameter * 2;
+
+  for (var col = 0; col < 5; col++) {
+
+    var ballCount = col + 1;
+    var x = 0.5 * eightball.PoolTable.width + col * ballRadius * Math.sqrt(3);
+    var yStart = -col * ballRadius;
+
+    for (var row = 0; row < ballCount; row++) {
+      eightball.PoolTable._createBall(world, x, yStart + row * ballRadius * 2, ballRadius);
+    }
+
+  }
+
+  cueBall = eightball.PoolTable._createBall(world, -0.5 * eightball.PoolTable.width, 0, ballRadius);
+};
+
+eightball.PoolTable._createTable = function(world) {
+
+  var matrixFlipHorizontal = new goog.math.Matrix([
+    [-1, 0],
+    [0, 1]]);
+
+  var matrixFlipVertical = new goog.math.Matrix([
+    [-1, 0],
+    [0, -1]]);
+
+  var table = new b2BodyDef();
+  table.friction = 0.5;
+
+  var side;
+  var points;
+
+  // Left
+  side = new b2PolyDef();
+  points = [
+    [-centerOffset.x, -centerOffset.y + eightball.PoolTable.bumperThickness * 2],
+    [-centerOffset.x + eightball.PoolTable.bumperThickness * 2, -centerOffset.y + eightball.PoolTable.bumperThickness * 4],
+    [-centerOffset.x + eightball.PoolTable.bumperThickness * 2, centerOffset.y - eightball.PoolTable.bumperThickness * 4],
+    [-centerOffset.x, centerOffset.y - eightball.PoolTable.bumperThickness * 2]];
+  side.SetVertices(points);
+  table.AddShape(side);
+
+  // Right
+  side = new b2PolyDef();
+  points = new goog.math.Matrix(points).multiply(matrixFlipHorizontal).toArray().reverse();
+  side.SetVertices(points);
+  table.AddShape(side);
+
+  // top left
+  points = [
+    [-centerOffset.x + eightball.PoolTable.bumperThickness * 2, -centerOffset.y],
+    [-centerOffset.x + eightball.PoolTable.bumperThickness * 4, -centerOffset.y + eightball.PoolTable.bumperThickness * 2],
+    [-eightball.PoolTable.bumperThickness * 2.2, -centerOffset.y + eightball.PoolTable.bumperThickness * 2],
+    [-eightball.PoolTable.bumperThickness * 2, -centerOffset.y]].reverse();
+
+  side = new b2PolyDef();
+  side.SetVertices(points);
+  table.AddShape(side);
+
+  // top right
+  side = new b2PolyDef();
+  points = new goog.math.Matrix(points).multiply(matrixFlipHorizontal).toArray().reverse();
+  side.SetVertices(points);
+  table.AddShape(side);
+
+  // bottom right
+  side = new b2PolyDef();
+  points = new goog.math.Matrix(points).multiply(matrixFlipVertical).toArray();
+  side.SetVertices(points);
+  table.AddShape(side);
+
+  // bottom left
+  side = new b2PolyDef();
+  points = new goog.math.Matrix(points).multiply(matrixFlipHorizontal).toArray().reverse();
+  side.SetVertices(points);
+  table.AddShape(side);
+
+  return world.CreateBody(table);
+};
+
+eightball.PoolTable._createBall = function(world, x, y, radius) {
+  var ballSd = new b2CircleDef();
+  ballSd.density = 1.0;
+  ballSd.radius = radius;
+  ballSd.restitution = 1;
+  ballSd.friction = 0.1;
+
+  var ballBd = new b2BodyDef();
+  ballBd.AddShape(ballSd);
+  ballBd.position.Set(x, y);
+  ballBd.linearDamping = 0.002;
+  ballBd.angularDamping = 0.005;
+  return world.CreateBody(ballBd);
+};
+
+eightball.PoolTable._step = function(cnt) {
+  var timeStep = 1.0 / 60;
+  var iteration = 1;
+  world.Step(timeStep, iteration);
+  canvasContext.clearRect(-centerOffset.x, -centerOffset.y, 2 * centerOffset.x, 2 * centerOffset.y);
+  eightball.PoolTable._drawWorld(world, canvasContext);
+  setTimeout('eightball.PoolTable._step(' + (cnt || 0) + ')', 10);
+};
+
+eightball.PoolTable._drawWorld = function(world, context) {
+  context.strokeStyle = '#ffffff';
+  context.beginPath();
+  context.moveTo(-10,-10);
+  context.lineTo(10,10);
+  context.moveTo(10,-10);
+  context.lineTo(-10,10);
+  context.stroke();
+
+  for (var b = world.m_bodyList; b; b = b.m_next) {
+    for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {
+      eightball.PoolTable._drawShape(s, context);
+    }
+  }
+};
+
+eightball.PoolTable._drawShape = function(shape, context) {
+  context.strokeStyle = '#ffffff';
+  context.beginPath();
+  switch (shape.m_type) {
+  case b2Shape.e_circleShape:
+    {
+      var circle = shape;
+      var pos = circle.m_position;
+      var r = circle.m_radius;
+      var segments = 16.0;
+      var theta = 0.0;
+      var dtheta = 2.0 * Math.PI / segments;
+      // draw circle
+      context.moveTo(pos.x + r, pos.y);
+      for (var i = 0; i < segments; i++) {
+        var d = new b2Vec2(r * Math.cos(theta), r * Math.sin(theta));
+        var v = b2Math.AddVV(pos, d);
+        context.lineTo(v.x, v.y);
+        theta += dtheta;
+      }
+      context.lineTo(pos.x + r, pos.y);
+  
+      // draw radius
+      context.moveTo(pos.x, pos.y);
+      var ax = circle.m_R.col1;
+      var pos2 = new b2Vec2(pos.x + r * ax.x, pos.y + r * ax.y);
+      context.lineTo(pos2.x, pos2.y);
+    }
+    break;
+  case b2Shape.e_polyShape:
+    {
+      var poly = shape;
+      var tV = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[0]));
+      context.moveTo(tV.x, tV.y);
+      for (var i = 0; i < poly.m_vertexCount; i++) {
+        var v = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[i]));
+        context.lineTo(v.x, v.y);
+      }
+      context.lineTo(tV.x, tV.y);
+    }
+    break;
+  }
+  context.stroke();
+};
--- public/javascripts/eightball/demos.js
+++ public/javascripts/eightball/demos.js
@@ -1,45 +1,23 @@
+goog.require('eightball.PoolTable');
+
 var world;
 var canvasContext;
-var canvasWidth;
-var canvasHeight;
 var centerOffset;
 var cueBall;
 
-var poolTable = {
-  // cm, regulation
-  height: 148,
-  // cm, regulation
-  width: 284,
-  // cm, regulation
-  ballDiameter: 5.715,
-  bumperThickness: 10
-};
-
-
-
-function step(cnt) {
-  var timeStep = 1.0 / 60;
-  var iteration = 1;
-  world.Step(timeStep, iteration);
-  canvasContext.clearRect(-centerOffset.x, -centerOffset.y, 2 * centerOffset.x, 2 * centerOffset.y);
-  drawWorld(world, canvasContext);
-  setTimeout('step(' + (cnt || 0) + ')', 10);
-}
 
 $(window).load(function() {
   var canvasElm = $('#demo_canvas');
   if (canvasElm) {
 
-    canvasElm.attr('width', poolTable.width * 2 + poolTable.bumperThickness * 4);
-    canvasElm.attr('height', poolTable.height * 2 + poolTable.bumperThickness * 4);
+    canvasElm.attr('width', eightball.PoolTable.width * 2 + eightball.PoolTable.bumperThickness * 4);
+    canvasElm.attr('height', eightball.PoolTable.height * 2 + eightball.PoolTable.bumperThickness * 4);
 
-    centerOffset = new b2Vec2(poolTable.width + poolTable.bumperThickness * 2, poolTable.height + poolTable.bumperThickness * 2);
-    world = createWorld();
+    centerOffset = new b2Vec2(eightball.PoolTable.width + eightball.PoolTable.bumperThickness * 2, eightball.PoolTable.height + eightball.PoolTable.bumperThickness * 2);
+
+    world = eightball.PoolTable.createWorld();
     canvasContext = canvasElm[0].getContext('2d');
     canvasContext.translate(centerOffset.x, centerOffset.y);
-
-    canvasWidth = canvasElm.width();
-    canvasHeight = canvasElm.height();
 
     canvasElm.click(function(e) {
       var velocity = new b2Vec2(b2Math.b2Random() , b2Math.b2Random() );
@@ -49,26 +27,6 @@ $(window).load(function() {
       cueBall.WakeUp();
     });
 
-    step();
+    eightball.PoolTable._step();
   }
 });
-
-
-
-function createBall(world, x, y, radius, initialVelocity) {
-  var ballSd = new b2CircleDef();
-  ballSd.density = 1.0;
-  ballSd.radius = radius;
-  ballSd.restitution = 1;
-  ballSd.friction = 0.1;
-
-  var ballBd = new b2BodyDef();
-  ballBd.AddShape(ballSd);
-  ballBd.position.Set(x, y);
-  if (initialVelocity) {
-    ballBd.linearVelocity = initialVelocity;
-  }
-  ballBd.linearDamping = 0.002;
-  ballBd.angularDamping = 0.005;
-  return world.CreateBody(ballBd);
-}
