35d3dad71794696a20f7874d13ecc5c238f8d053
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-07-20 01:06:57 +0000

    Starting pocket work...
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@50 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- public/javascripts/eightball/demo_base.js
+++ public/javascripts/eightball/demo_base.js
@@ -1,41 +1,49 @@
 function createWorld() {
-  function setupBalls(world){
+  function setupBalls(world) {
     var ballRadius = poolTable.ballDiameter * 2;
 
-    for(var col=0;col<5;col++){
+    for (var col = 0; col < 5; col++) {
 
-      var ballCount = col+1;
+      var ballCount = col + 1;
       var x = 1.5 * poolTable.width + col * ballRadius * Math.sqrt(3);
       var yStart = 2 * poolTable.bumperThickness + poolTable.height - col * ballRadius;
 
-      for(var row=0;row<ballCount;row++){
-        createBall(world, x, yStart + row * ballRadius*2, ballRadius);
+      for (var row = 0; row < ballCount; row++) {
+        createBall(world, x, yStart + row * ballRadius * 2, ballRadius);
       }
 
     }
   }
 
+
+
   function createTable(world) {
-    var sideLeft = new b2BoxDef();
-    sideLeft.extents.Set(poolTable.bumperThickness, poolTable.height);
-    sideLeft.localPosition.Set(poolTable.bumperThickness, poolTable.height+poolTable.bumperThickness*2);
+    var sideLeft = new b2PolyDef();
+    sideLeft.SetVertices([
+      [0, poolTable.bumperThickness * 2],
+      [poolTable.bumperThickness * 2, poolTable.bumperThickness * 4],
+      [poolTable.bumperThickness * 2, poolTable.height * 2],
+      [0, poolTable.height * 2 + poolTable.bumperThickness * 2]]);
 
     var sideRight = new b2BoxDef();
     sideRight.extents.Set(poolTable.bumperThickness, poolTable.height);
-    sideRight.localPosition.Set(poolTable.width*2+poolTable.bumperThickness*3, poolTable.height+poolTable.bumperThickness*2);
+    sideRight.localPosition.Set(poolTable.width * 2 + poolTable.bumperThickness * 3, poolTable.height + poolTable.bumperThickness * 2);
 
-    var sideTop = new b2BoxDef();
-    sideTop.extents.Set(poolTable.width, poolTable.bumperThickness);
-    sideTop.localPosition.Set(poolTable.width+poolTable.bumperThickness*2, poolTable.bumperThickness)
+    var sideTopLeft = new b2PolyDef();
+    sideTopLeft.SetVertices([
+      [poolTable.width, 0],
+      [poolTable.width, poolTable.bumperThickness * 2],
+      [poolTable.bumperThickness * 4, poolTable.bumperThickness * 2],
+      [poolTable.bumperThickness * 2, 0]]);
 
     var sideBottom = new b2BoxDef();
     sideBottom.extents.Set(poolTable.width, poolTable.bumperThickness);
-    sideBottom.localPosition.Set(poolTable.width+poolTable.bumperThickness*2, poolTable.height*2+poolTable.bumperThickness*3)
+    sideBottom.localPosition.Set(poolTable.width + poolTable.bumperThickness * 2, poolTable.height * 2 + poolTable.bumperThickness * 3);
 
     var table = new b2BodyDef();
     table.AddShape(sideLeft);
+    table.AddShape(sideTopLeft);
     table.AddShape(sideRight);
-    table.AddShape(sideTop);
     table.AddShape(sideBottom);
 
     return world.CreateBody(table);
