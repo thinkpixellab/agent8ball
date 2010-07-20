bde0f8873a75d4a89f7a7311115887e6029017a3
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-07-20 01:07:17 +0000

    Clean-up of box2d math files
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@51 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- public/javascripts/box2d/common/math/b2Mat22.js
+++ public/javascripts/box2d/common/math/b2Mat22.js
@@ -16,79 +16,72 @@
 * 3. This notice may not be removed or altered from any source distribution.
 */
 
-
-
-
-
 var b2Mat22 = Class.create();
-b2Mat22.prototype = 
-{
-  initialize: function(angle, c1, c2)
-  {
-    if (angle==null) angle = 0;
+b2Mat22.prototype = {
+  initialize: function(angle, c1, c2) {
+    if (angle == null) angle = 0;
     // initialize instance variables for references
     this.col1 = new b2Vec2();
     this.col2 = new b2Vec2();
     //
-
-    if (c1!=null && c2!=null){
+    if (c1 != null && c2 != null) {
       this.col1.SetV(c1);
       this.col2.SetV(c2);
-    }
-    else{
+    } else {
       var c = Math.cos(angle);
       var s = Math.sin(angle);
-      this.col1.x = c; this.col2.x = -s;
-      this.col1.y = s; this.col2.y = c;
+      this.col1.x = c;
+      this.col2.x = -s;
+      this.col1.y = s;
+      this.col2.y = c;
     }
   },
 
-  Set: function(angle)
-  {
+  Set: function(angle) {
     var c = Math.cos(angle);
     var s = Math.sin(angle);
-    this.col1.x = c; this.col2.x = -s;
-    this.col1.y = s; this.col2.y = c;
+    this.col1.x = c;
+    this.col2.x = -s;
+    this.col1.y = s;
+    this.col2.y = c;
   },
 
-  SetVV: function(c1, c2)
-  {
+  SetVV: function(c1, c2) {
     this.col1.SetV(c1);
     this.col2.SetV(c2);
   },
 
-  Copy: function(){
+  Copy: function() {
     return new b2Mat22(0, this.col1, this.col2);
   },
 
-  SetM: function(m)
-  {
+  SetM: function(m) {
     this.col1.SetV(m.col1);
     this.col2.SetV(m.col2);
   },
 
-  AddM: function(m)
-  {
+  AddM: function(m) {
     this.col1.x += m.col1.x;
     this.col1.y += m.col1.y;
     this.col2.x += m.col2.x;
     this.col2.y += m.col2.y;
   },
 
-  SetIdentity: function()
-  {
-    this.col1.x = 1.0; this.col2.x = 0.0;
-    this.col1.y = 0.0; this.col2.y = 1.0;
+  SetIdentity: function() {
+    this.col1.x = 1.0;
+    this.col2.x = 0.0;
+    this.col1.y = 0.0;
+    this.col2.y = 1.0;
   },
 
-  SetZero: function()
-  {
-    this.col1.x = 0.0; this.col2.x = 0.0;
-    this.col1.y = 0.0; this.col2.y = 0.0;
+  SetZero: function() {
+    this.col1.x = 0.0;
+    this.col2.x = 0.0;
+    this.col1.y = 0.0;
+    this.col2.y = 0.0;
   },
 
-  Invert: function(out)
-  {
+  Invert: function(out) {
     var a = this.col1.x;
     var b = this.col2.x;
     var c = this.col1.y;
@@ -97,14 +90,15 @@ b2Mat22.prototype =
     var det = a * d - b * c;
     //b2Settings.b2Assert(det != 0.0);
     det = 1.0 / det;
-    out.col1.x =  det * d;  out.col2.x = -det * b;
-    out.col1.y = -det * c;  out.col2.y =  det * a;
+    out.col1.x = det * d;
+    out.col2.x = -det * b;
+    out.col1.y = -det * c;
+    out.col2.y = det * a;
     return out;
   },
 
   // this.Solve A * x = b
-  Solve: function(out, bX, bY)
-  {
+  Solve: function(out, bX, bY) {
     //float32 a11 = this.col1.x, a12 = this.col2.x, a21 = this.col1.y, a22 = this.col2.y;
     var a11 = this.col1.x;
     var a12 = this.col2.x;
@@ -120,11 +114,11 @@ b2Mat22.prototype =
     return out;
   },
 
-  Abs: function()
-  {
+  Abs: function() {
     this.col1.Abs();
     this.col2.Abs();
   },
 
   col1: new b2Vec2(),
-  col2: new b2Vec2()};
+  col2: new b2Vec2()
+};
--- public/javascripts/box2d/common/math/b2Vec2.js
+++ public/javascripts/box2d/common/math/b2Vec2.js
@@ -16,99 +16,97 @@
 * 3. This notice may not be removed or altered from any source distribution.
 */
 
-
-
-
-
 // b2Vec2 has no constructor so that it
 // can be placed in a union.
 var b2Vec2 = Class.create();
-b2Vec2.prototype = 
-{
-  initialize: function(x_, y_) {this.x=x_; this.y=y_;},
-
-  SetZero: function() { this.x = 0.0; this.y = 0.0; },
-  Set: function(x_, y_) {this.x=x_; this.y=y_;},
-  SetV: function(v) {this.x=v.x; this.y=v.y;},
-
-  Negative: function(){ return new b2Vec2(-this.x, -this.y); },
-
-
-  Copy: function(){
-    return new b2Vec2(this.x,this.y);
+b2Vec2.prototype = {
+  initialize: function(x_, y_) {
+    this.x = x_;
+    this.y = y_;
   },
 
-  Add: function(v)
-  {
-    this.x += v.x; this.y += v.y;
+  SetZero: function() {
+    this.x = 0.0;
+    this.y = 0.0;
+  },
+  Set: function(x_, y_) {
+    this.x = x_;
+    this.y = y_;
+  },
+  SetV: function(v) {
+    this.x = v.x;
+    this.y = v.y;
   },
 
-  Subtract: function(v)
-  {
-    this.x -= v.x; this.y -= v.y;
+  Negative: function() {
+    return new b2Vec2(-this.x, -this.y);
   },
 
-  Multiply: function(a)
-  {
-    this.x *= a; this.y *= a;
+  Copy: function() {
+    return new b2Vec2(this.x, this.y);
   },
 
-  MulM: function(A)
-  {
+  Add: function(v) {
+    this.x += v.x;
+    this.y += v.y;
+  },
+
+  Subtract: function(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+  },
+
+  Multiply: function(a) {
+    this.x *= a;
+    this.y *= a;
+  },
+
+  MulM: function(A) {
     var tX = this.x;
     this.x = A.col1.x * tX + A.col2.x * this.y;
     this.y = A.col1.y * tX + A.col2.y * this.y;
   },
 
-  MulTM: function(A)
-  {
+  MulTM: function(A) {
     var tX = b2Math.b2Dot(this, A.col1);
     this.y = b2Math.b2Dot(this, A.col2);
     this.x = tX;
   },
 
-  CrossVF: function(s)
-  {
+  CrossVF: function(s) {
     var tX = this.x;
     this.x = s * this.y;
     this.y = -s * tX;
   },
 
-  CrossFV: function(s)
-  {
+  CrossFV: function(s) {
     var tX = this.x;
     this.x = -s * this.y;
     this.y = s * tX;
   },
 
-  MinV: function(b)
-  {
+  MinV: function(b) {
     this.x = this.x < b.x ? this.x : b.x;
     this.y = this.y < b.y ? this.y : b.y;
   },
 
-  MaxV: function(b)
-  {
+  MaxV: function(b) {
     this.x = this.x > b.x ? this.x : b.x;
     this.y = this.y > b.y ? this.y : b.y;
   },
 
-  Abs: function()
-  {
+  Abs: function() {
     this.x = Math.abs(this.x);
     this.y = Math.abs(this.y);
   },
 
-  Length: function()
-  {
+  Length: function() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   },
 
-  Normalize: function()
-  {
+  Normalize: function() {
     var length = this.Length();
-    if (length < Number.MIN_VALUE)
-    {
+    if (length < Number.MIN_VALUE) {
       return 0.0;
     }
     var invLength = 1.0 / length;
@@ -118,14 +116,13 @@ b2Vec2.prototype =
     return length;
   },
 
-  IsValid: function()
-  {
+  IsValid: function() {
     return b2Math.b2IsValid(this.x) && b2Math.b2IsValid(this.y);
   },
 
   x: null,
-  y: null};
-b2Vec2.Make = function(x_, y_)
-  {
-    return new b2Vec2(x_, y_);
-  };
+  y: null
+};
+b2Vec2.Make = function(x_, y_) {
+  return new b2Vec2(x_, y_);
+};
