d2a6f7ee7c888622ac428aa1d032bc132bf146de
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-07-29 17:24:26 +0000

    Moving some functionality around
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@94 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,26 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+goog.require('eightball.PoolTable');
+
+var poolTable;
+
+$(window).load(function () {
+    var canvasElement = $('canvas#demo_canvas');
+    var cueCanvasElement = $('canvas#cue_canvas');
+    if (canvasElement[0]) {
+        poolTable = new eightball.PoolTable(canvasElement, cueCanvasElement);
+
+        width = window.innerWidth;
+        height = window.innerHeight;
+        poolTable.updateLayout(width, height);
+    }
+});
+
+
+
+$(window).resize(function (e) {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    poolTable.updateLayout(width, height);
+});
