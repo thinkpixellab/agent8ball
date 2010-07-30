edc80b77def3c2a91df2253c29bc6e7c11260395
ingebretsen <ingebretsen@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-07-30 04:34:43 +0000

    added some basic sound support (plays the background music) and controls for turning it on and off. also added a jquery cookie plugin (cookie.js).
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@109 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

---
+++ public/javascripts/application.js
@@ -0,0 +1,83 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+goog.require('eightball.PoolTable');
+goog.require('eightball.Sounds');
+
+var poolTable;
+var soundManager;
+
+$(window).load(function () {
+
+    // create our new sound manager
+    soundManager = new eightball.Sounds($("audio#musicelement"));
+
+    var canvasElement = $('canvas#demo_canvas');
+    var cueCanvasElement = $('canvas#cue_canvas');
+
+    if (canvasElement[0]) {
+        poolTable = new eightball.PoolTable(canvasElement, cueCanvasElement);
+
+        width = window.innerWidth;
+        height = window.innerHeight;
+        poolTable.updateLayout(width, height);
+    }
+
+    // music on/off
+    $("#musicbutton").click(function () {
+        if (soundManager.isMusicOn) {
+            $("#musicbuttonon").fadeOut("fast", function () {
+                soundManager.stopMusic();
+                $.cookie("music", "nope");
+            });
+        }
+        else {
+            $("#musicbuttonon").fadeIn("fast", function () {
+                soundManager.startMusic("sounds/theme.mp4", true);
+                $.cookie("music", "yup");
+            });
+        }
+    });
+
+    // sound effects on/off
+    $("#soundsbutton").click(function () {
+        if (soundManager.isSoundEnabled) {
+            $("#soundsbuttonon").fadeOut("fast", function () {
+                soundManager.isSoundEnabled = false;
+            });
+        }
+        else {
+            $("#soundsbuttonon").fadeIn("fast", function () {
+                soundManager.isSoundEnabled = true;
+            });
+        }
+    });
+
+    // start the music (but check our cookie first)
+    var musicCookie = $.cookie("music");
+    if (musicCookie != "off") {
+        soundManager.startMusic("sounds/theme.mp4", true);
+    }
+    else {
+        $("#musicbuttonon").fadeOut("fast", function () {
+            soundManager.stopMusic();
+        });
+    }
+
+});
+
+// document interaction
+
+$(document).ready(function () {
+
+});
+
+$(window).resize(function (e) {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    poolTable.updateLayout(width, height);
+});
+
+// enableDebug();
+
+preload("/images/bestscore.png,/images/cue.png,/images/progressbg.png,/images/progressunit.png,/images/score.png,/images/table.jpg,/images/tableborder.png,/images/timeremaining.png,/images/wood.jpg");
