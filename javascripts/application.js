eaa56f3ef6bb12ef6e6bb9670fb47aece5b97b92
ingebretsen <ingebretsen@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-07-30 05:09:01 +0000

    making changes in the root now instead of public. can we nuke public?
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@110 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,14 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 goog.require('eightball.PoolTable');
+goog.require('eightball.Sounds');
 goog.require('pixelLab.ImagePreloader');
 goog.require('pixelLab.Debug');
 
 var poolTable;
+var soundManager;
 
-$(window).load(function() {
+$(window).load(function () {
   pixelLab.Debug.enable();
   pixelLab.ImagePreloader.preload("images/bestscore.png, images/cue.png, images/progressbg.png, images/progressunit.png, images/score.png, images/table.jpg, images/tableborder.png, images/timeremaining.png, images/wood.jpg");
+
+  // create our new sound manager
+  soundManager = new eightball.Sounds($("audio#musicelement"));
+
   var canvasElement = $('canvas#demo_canvas');
   var cueCanvasElement = $('canvas#cue_canvas');
   if (canvasElement[0]) {
@@ -18,9 +24,50 @@ $(window).load(function() {
     height = window.innerHeight;
     poolTable.updateLayout(width, height);
   }
+
+  // music on/off
+  $("#musicbutton").click(function () {
+    if (soundManager.isMusicOn) {
+      $("#musicbuttonon").fadeOut("fast", function () {
+        soundManager.stopMusic();
+        $.cookie("music", "nope");
+      });
+    }
+    else {
+      $("#musicbuttonon").fadeIn("fast", function () {
+        soundManager.startMusic("sounds/theme.mp4", true);
+        $.cookie("music", "yup");
+      });
+    }
+  });
+
+  // sound effects on/off
+  $("#soundsbutton").click(function () {
+    if (soundManager.isSoundEnabled) {
+      $("#soundsbuttonon").fadeOut("fast", function () {
+        soundManager.isSoundEnabled = false;
+      });
+    }
+    else {
+      $("#soundsbuttonon").fadeIn("fast", function () {
+        soundManager.isSoundEnabled = true;
+      });
+    }
+  });
+
+  // start the music (but check our cookie first)
+  var musicCookie = $.cookie("music");
+  if (musicCookie != "off") {
+    soundManager.startMusic("sounds/theme.mp4", true);
+  }
+  else {
+    $("#musicbuttonon").fadeOut("fast", function () {
+      soundManager.stopMusic();
+    });
+  }
 });
 
-$(window).resize(function(e) {
+$(window).resize(function (e) {
   width = window.innerWidth;
   height = window.innerHeight;
   poolTable.updateLayout(width, height);
---
+++ javascripts/eightball/Sounds.js
@@ -0,0 +1,58 @@
+﻿goog.provide('eightball.Sounds');
+goog.provide('eightball.SoundEffect');
+
+/**
+@constructor
+*/
+eightball.Sounds = function () {
+    this.isMusicOn = false;
+    this.isSoundEnabled = false;
+    this.music = null;
+};
+
+eightball.Sounds.prototype.startMusic = function (location, loop) {
+
+    this.stopMusic();
+
+    // stop the current music and remove the audio element
+    if (this.music) {
+        this.music.pause();
+        this.music.parentNode.removeChild(this.music);
+        this.music = null;
+    }
+
+    // check our inputs
+    if (location == undefined || location == null) return;
+    if (loop == undefined || loop == null) loop = false;
+
+    // create the audio element
+    this.music = document.createElement("audio");
+    this.music.setAttribute("src", location);
+    this.music.play();
+    this.isMusicOn = true;
+
+    // set option parameters
+    if (loop) this.music.setAttribute("loop", "loop")
+
+    // add it to the document
+    document.body.appendChild(this.music);
+
+}
+
+eightball.Sounds.prototype.stopMusic = function () {
+
+    // stop the current music and remove the audio element
+    if (this.music) {
+        this.music.pause();
+        this.music.parentNode.removeChild(this.music);
+        this.music = null;
+    }
+
+    this.isMusicOn = false;
+}
+
+
+eightball.SoundEffect = function () {
+
+
+}
