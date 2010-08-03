8015adb1dadb84a21cfa571c0dcdb707826cfbf3
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-03 15:07:29 +0000

    Compile...
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@134 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- make_deps.py
+++ make_deps.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-import logging
 import os
+import fnmatch
 from subprocess import call
 
 js_path = "javascripts"
 closure_path = os.path.join(js_path, 'closure-library','closure')
-calcdeps_js_path = os.path.join(closure_path, "bin/calcdeps.py")
 application_js_path = os.path.join(js_path, 'application.js')
-deps_js_path = os.path.join(js_path, "deps.js")
 js_dirs = ['box2d','eightball','helpers']
+
+# deps
+calcdeps_js_path = os.path.join(closure_path, "bin/calcdeps.py")
+deps_js_path = os.path.join(js_path, "deps.js")
+
+# compile
+compiled_js_path = os.path.join(js_path, "compiled.js")
+jar_path = os.path.join('_tools', 'closure_compiler', 'compiler.jar')
 
 def make_deps():
   
@@ -24,8 +30,52 @@ def make_deps():
   
   for js_dir in js_dirs:
     command += " -p %s" % os.path.join(js_path, js_dir)
-
+  
   return command
+
+def compile():
+  command = "java"
+  command += " -jar %s" % jar_path
+  
+  files = []
+  # add js files in goog dir, without files in demos
+  for file in find_files(closure_path, '*.js'):
+    if(file.find('demos') == -1):
+      files.append(file)
+  
+  # add all js files in each of js_dirs
+  for js_dir in js_dirs:
+    js_dir = os.path.join(js_path, js_dir)
+    for file in find_files(js_dir, '*.js'):
+      files.append(file)
+  
+  files.append(os.path.join(js_path, 'application.js'))
+  
+  for file in files:
+    command += " --js %s" % file
+  
+  #  command += " --compilation_level ADVANCED_OPTIMIZATIONS"
+  command += " --compilation_level SIMPLE_OPTIMIZATIONS"
+  command += " --summary_detail_level 3"
+  # debug makes var names readabel, but was causing weirdness..
+  command += " --debug true"
+  command += " --warning_level VERBOSE"
+  # make sure everything is in a good order
+  command += " --manage_closure_dependencies true"
+  command += " --jscomp_dev_mode EVERY_PASS"
+  command += " --js_output_file %s" % compiled_js_path
+  
+  command += " --formatting PRETTY_PRINT"
+  command += " --formatting PRINT_INPUT_DELIMITER"
+  
+  return command
+
+def find_files(directory, pattern):
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = os.path.join(root, basename)
+                yield filename
 
 if __name__ == '__main__':
   command = compile()
