9217ed3f76726df332a59c81c0318640f4c62301
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-04 16:46:03 +0000

    Game.js fixes
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@154 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- javascripts/eightball/Game.js
+++ javascripts/eightball/Game.js
@@ -11,39 +11,32 @@ goog.require('goog.object');
 goog.require('goog.net.cookies');
 
 /**
-@const
-@type {number}
-*/
-eightball.Game.s_gameSeconds = 120;
-
-/**
-@constructor
-@extends {goog.events.EventTarget}
-*/
-eightball.Game = function () {
+ @constructor
+ @extends {goog.events.EventTarget}
+ */
+eightball.Game = function() {
 
   this.gameState = eightball.Music.GameState.READY;
-  this.secondsLeft = 120; //eightball.Game.s_gameSeconds;
+  this.secondsLeft = eightball.Game.s_gameSeconds;
 
   /**
-  @private
-  */
+   @private
+   */
   this.m_timer = null;
-
 
 };
 goog.inherits(eightball.Game, goog.events.EventTarget);
 
-eightball.Game.prototype.start = function () {
+eightball.Game.prototype.start = function() {
 
   // reset the timer and our clock
   if (this.m_timer) {
     this.m_timer.dispose();
-    this.secondsLeft = 120; //eightball.Game.s_gameSeconds;
+    this.secondsLeft = eightball.Game.s_gameSeconds;
   }
 
   // create a new timer
-  this.m_timer = new goog.Timer(this._inMs(1));
+  this.m_timer = new goog.Timer(eightball.Game._inMs(1));
 
   // start the timer
   this.m_timer.start();
@@ -54,106 +47,116 @@ eightball.Game.prototype.start = function () {
 
 };
 
-eightball.Game.prototype.togglePaused = function () {
+eightball.Game.prototype.togglePaused = function() {
 
-  if (this.gameState == eightball.Music.GameState.STARTED){
+  if (this.gameState == eightball.Music.GameState.STARTED) {
     this.gameState = eightball.Music.GameState.PAUSED;
     this._dispatchGameEvent(eightball.Game.EventType.PAUSE);
-  }
-
-  else if (this.gameState == eightball.Music.GameState.PAUSED) {
+  } else if (this.gameState == eightball.Music.GameState.PAUSED) {
     this.gameState = eightball.Music.GameState.STARTED;
     this._dispatchGameEvent(eightball.Game.EventType.RESUME);
   }
-
+  // TODO: else?
 };
 
-eightball.Game.prototype._tickAction = function () {
+eightball.Game.prototype._tickAction = function() {
   if (this.gameState == eightball.Music.GameState.STARTED) {
-    
+
     this.secondsLeft--;
 
     if (this.secondsLeft <= 0) {
       this.m_timer.stop();
       this.gameState = eightball.Music.GameState.ENDED;
       this._dispatchGameEvent(eightball.Game.EventType.END);
-    }
-    else {
+    } else {
       this._dispatchGameEvent(eightball.Game.EventType.TICK);
     }
 
   }
 };
 
-eightball.Game.prototype._dispatchGameEvent = function (type) {
+/**
+  @private
+*/
+eightball.Game.prototype._dispatchGameEvent = function(type) {
   this.dispatchEvent(new goog.events.Event(type, this));
 };
 
-eightball.Game.prototype._inMs = function (seconds) {
+/**
+  @private
+  @param {number} seconds
+  @returns {number}
+*/
+eightball.Game._inMs = function(seconds) {
   return seconds * 1000;
 };
 
 /** 
-* Possible game states
-* @enum {string}
-*/
+ * Possible game states
+ * @enum {string}
+ */
 eightball.Music.GameState = {
   /**
-  * The game is ready to be played.
-  */
+   * The game is ready to be played.
+   */
   READY: 'ready',
 
   /**
-  * The game has been started and is in play.
-  */
+   * The game has been started and is in play.
+   */
   STARTED: 'started',
 
   /**
-  * The game has been started and is in play.
-  */
+   * The game has been started and is in play.
+   */
   PAUSED: 'paused',
 
   /**
-  * The game has ended.
-  */
+   * The game has ended.
+   */
   ENDED: 'ended'
 
 };
 
-
 /**
-* Events fired by the game.
-* @enum {string}
-*/
+ * Events fired by the game.
+ * @enum {string}
+ */
 eightball.Game.EventType = {
   /**
-  * Dispatched when the game is ready to be played (presumably after the preloader has downloaded assets, etc.)
-  */
+   * Dispatched when the game is ready to be played (presumably after the preloader has downloaded assets, etc.)
+   */
   READY: 'ready',
 
   /**
-  * Dispatched when the game is started.
-  */
+   * Dispatched when the game is started.
+   */
   START: 'start',
 
   /**
-  * Dispatched when the game is paused.
-  */
+   * Dispatched when the game is paused.
+   */
   PAUSE: 'pause',
 
   /**
-  * Dispatched when the game is resumed aftering being paused.
-  */
+   * Dispatched when the game is resumed aftering being paused.
+   */
   RESUME: 'resume',
 
   /**
-  * Dispatched when the game comes to an end (because the timer has reaced a value of 0).
-  */
+   * Dispatched when the game comes to an end (because the timer has reaced a value of 0).
+   */
   END: 'end',
 
   /**
-  * Dispatched when the game timer is updated. Occurs once per second.
-  */
+   * Dispatched when the game timer is updated. Occurs once per second.
+   */
   TICK: 'tick'
 };
 
+/**
+ @private
+ @const
+ @type {number}
+ */
+eightball.Game.s_gameSeconds = 120;
