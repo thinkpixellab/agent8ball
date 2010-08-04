3e376ffb318c1c1512524f62adcd96029d8ea724
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-04 00:36:02 +0000

    Added html to jquery extern file
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@147 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- javascripts/externs/jquery.js
+++ javascripts/externs/jquery.js
@@ -1,3 +1,5 @@
+// copied as needed from
+// http://closure-compiler.googlecode.com/svn/trunk/contrib/externs/jquery-1.3.2.externs.js
 /**
  * @param {Object|string} arg
  * @return {jQueryObject}
@@ -51,3 +53,9 @@ jQueryObject.prototype.fadeIn = function(opt_speed, opt_fn) {};
  * @return {jQueryObject}
  */
 jQueryObject.prototype.fadeOut = function(opt_speed, opt_fn) {};
+
+/**
+ * @param {string} opt_val
+ * @return {string|jQueryObject}
+ */
+jQueryObject.prototype.html = function(opt_val) {};
