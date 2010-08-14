2d06ae387c01758af91cca2b00de84b52e615c03
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-14 01:14:53 +0000

    Small tweaks to b2WorldListener
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@222 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- javascripts/box2d/dynamics/b2WorldListener.js
+++ javascripts/box2d/dynamics/b2WorldListener.js
@@ -18,27 +18,27 @@
 
 goog.provide('b2WorldListener');
 
-/** @constructor */
+/**
+ @constructor
+ */
 b2WorldListener = function() {};
-b2WorldListener.prototype = {
 
-  // If a body is destroyed, then any joints attached to it are also destroyed.
-  // This prevents memory leaks, but you may unexpectedly be left with an
-  // orphaned joint pointer.
-  // Box2D will notify you when a joint is implicitly destroyed.
-  // It is NOT called if you directly destroy a joint.
-  // Implement this abstract class and provide it to b2World via
-  // b2World::SetListener().
-  // DO NOT modify the Box2D world inside this callback.
-  NotifyJointDestroyed: function(joint) {},
+// If a body is destroyed, then any joints attached to it are also destroyed.
+// This prevents memory leaks, but you may unexpectedly be left with an
+// orphaned joint pointer.
+// Box2D will notify you when a joint is implicitly destroyed.
+// It is NOT called if you directly destroy a joint.
+// Implement this abstract class and provide it to b2World via
+// b2World::SetListener().
+// DO NOT modify the Box2D world inside this callback.
+b2WorldListener.prototype.NotifyJointDestroyed = function(joint) {};
 
-  // This is called when a body's shape passes outside of the world boundary. If you
-  // override this and pass back e_destroyBody, you must nullify your copies of the
-  // body pointer.
-  NotifyBoundaryViolated: function(body) {
-    //NOT_USED(body);
-    return b2WorldListener.b2_freezeBody;
-  }
+// This is called when a body's shape passes outside of the world boundary. If you
+// override this and pass back e_destroyBody, you must nullify your copies of the
+// body pointer.
+b2WorldListener.prototype.NotifyBoundaryViolated = function(body) {
+  //NOT_USED(body);
+  return b2WorldListener.b2_freezeBody;
 };
 b2WorldListener.b2_freezeBody = 0;
 b2WorldListener.b2_destroyBody = 1;
