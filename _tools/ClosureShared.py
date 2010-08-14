a8a861eca6204870845af768f8faad8577131ae0
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-14 23:32:26 +0000

    Closure script clean-up
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@230 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- _tools/ClosureShared.py
+++ _tools/ClosureShared.py
@@ -6,11 +6,12 @@ import logging
 import subprocess
 import datetime
 
-current_file_path = os.path.abspath( __file__ )
-current_file_dir = os.path.dirname(current_file_path)
-jar_path = os.path.join(current_file_dir, 'closure_compiler', 'compiler.jar')
+current_file_dir = os.path.relpath(os.path.dirname(__file__))
+jar_path = os.path.join(current_file_dir, 'compiler.jar')
+closure_path = calcdeps_py_path = os.path.join(current_file_dir, 'closure-library', 'closure')
+calcdeps_py_path = os.path.join(closure_path, 'bin', 'calcdeps.py')
 
-def make_deps_core(calcdeps_py_path, deps_js_path, closure_path, js_dirs):
+def make_deps_core(deps_js_path, js_dirs):
 
   command = ['python']
   command += [calcdeps_py_path]
@@ -26,10 +27,8 @@ def make_deps_core(calcdeps_py_path, deps_js_path, closure_path, js_dirs):
   return command, tmp_file_path, deps_js_path
 
 class Closure:
-  def __init__(self, closure_path, application_js_path, closure_dependencies, calcdeps_py_path, deps_js_path, compiled_js_path, extern_dir, debug = False):
-    self.calcdeps_py_path = calcdeps_py_path
+  def __init__(self, application_js_path, closure_dependencies, deps_js_path, compiled_js_path, extern_dir, debug = False):
     self.deps_js_path = deps_js_path
-    self.closure_path = closure_path
     self.closure_dependencies = closure_dependencies
     self.application_js_path = application_js_path
     self.extern_dir = extern_dir
@@ -56,10 +55,10 @@ class Closure:
       os.rename(tmp_file, out_file)
 
   def make_deps(self):
-    return make_deps_core(self.calcdeps_py_path, self.deps_js_path, self.closure_path, self.closure_dependencies)
+    return make_deps_core(self.deps_js_path, self.closure_dependencies)
 
   def get_compile_files(self):
-    js_files = get_js_files_for_compile(self.application_js_path, self.deps_js_path, self.closure_path)
+    js_files = get_js_files_for_compile(self.application_js_path, self.deps_js_path)
   
     extern_files = []
     for file in find_files(self.extern_dir, '*.js'):
@@ -69,15 +68,11 @@ class Closure:
 
   def compile(self):
     js_files, extern_files = self.get_compile_files()
-    debug = False
-    return compile_core(jar_path, self.closure_path, js_files, extern_files, self.compiled_js_path, self.debug)
+    return compile_core(js_files, extern_files, self.compiled_js_path, self.debug)
 
-def print_tree():
-  js_files, extern_files = get_compile_files()
-  return ClosureShared.print_tree(jar_path, closure_path, js_files, extern_files)
-
-def print_help():
-  return ClosureShared.print_help(jar_path)
+  # def print_tree(self):
+  #   js_files, extern_files = self.get_compile_files()
+  #   return print_tree_core(js_files, extern_files)
 
 def get_tmp_file_name(source_file_name):
   name = source_file_name
@@ -85,10 +80,10 @@ def get_tmp_file_name(source_file_name):
   name += datetime.datetime.utcnow().isoformat().replace(':','_')
   return name
 
-def get_closure_base(jar_path):
+def get_closure_base():
   return ["java", "-jar", jar_path]
 
-def get_closure_inputs(closure_path, js_files, extern_files):
+def get_closure_inputs(js_files, extern_files):
   command_inputs = []
   
   command_inputs += ["--js", os.path.join(closure_path, 'goog', 'base.js')]
@@ -102,7 +97,7 @@ def get_closure_inputs(closure_path, js_files, extern_files):
   command_inputs += ["--manage_closure_dependencies", "true"]
   return command_inputs
 
-def get_js_files_for_compile(app_file, app_dep_file, closure_path):
+def get_js_files_for_compile(app_file, app_dep_file):
   dep_files = [app_dep_file]
   dep_files.append(os.path.join(closure_path, 'goog', 'deps.js'))
   
@@ -110,7 +105,7 @@ def get_js_files_for_compile(app_file, app_dep_file, closure_path):
   file_to_require_hash = {}
   
   for dep_file in dep_files:
-    process_deps(dep_file, provide_to_file_hash, file_to_require_hash, closure_path)
+    process_deps(dep_file, provide_to_file_hash, file_to_require_hash)
   
   files = []
   populate_files(app_file, files, provide_to_file_hash, file_to_require_hash)
@@ -123,7 +118,7 @@ def get_js_files_for_compile(app_file, app_dep_file, closure_path):
   
   return files
 
-def get_goog_js_files(closure_path):
+def get_goog_js_files():
   files = []
   # add js files in goog dir, without files in demos
   for file in find_files(closure_path, '*.js'):
@@ -131,11 +126,11 @@ def get_goog_js_files(closure_path):
       files.append(file)
   return files
 
-def get_command_with_inputs(jar_path, closure_path, js_files, extern_files):
-  return get_closure_base(jar_path) + get_closure_inputs(closure_path, js_files, extern_files)
+def get_command_with_inputs(js_files, extern_files):
+  return get_closure_base() + get_closure_inputs(js_files, extern_files)
 
-def compile_core(jar_path, closure_path, js_files, extern_files, compiled_js_path, debug=False):
-  command = get_command_with_inputs(jar_path, closure_path, js_files, extern_files)
+def compile_core(js_files, extern_files, compiled_js_path, debug=False):
+  command = get_command_with_inputs(js_files, extern_files)
   
   command += ["--compilation_level", "ADVANCED_OPTIMIZATIONS"] # SIMPLE_OPTIMIZATIONS
   command += ["--summary_detail_level", "3"]
@@ -160,7 +155,7 @@ def find_files(directory, pattern):
                 filename = os.path.join(root, basename)
                 yield filename
 
-def run_addDependency(file_path, provided, required, provide_to_file_hash, file_to_require_hash, closure_path):
+def run_addDependency(file_path, provided, required, provide_to_file_hash, file_to_require_hash):
   file_path = os.path.join(closure_path, 'goog', file_path)
   file_path = os.path.normpath(file_path)
   if(file_path in file_to_require_hash):
@@ -171,17 +166,17 @@ def run_addDependency(file_path, provided, required, provide_to_file_hash, file_
       raise Exception("I've already seen '%s'" % symbol)
     provide_to_file_hash[symbol] = file_path
 
-def process_line(line, provide_to_file_hash, file_to_require_hash, closure_path):
-  locals = {'addDependency': lambda x, y, z: run_addDependency(x, y, z, provide_to_file_hash, file_to_require_hash, closure_path) }
+def process_line(line, provide_to_file_hash, file_to_require_hash):
+  locals = {'addDependency': lambda x, y, z: run_addDependency(x, y, z, provide_to_file_hash, file_to_require_hash) }
   line = line.strip()
   if(line.startswith('goog.addDependency')):
     line = string.replace(line, 'goog.addDependency', 'addDependency')
     line = string.replace(line, ';', '')
     eval(line, {}, locals)
 
-def process_deps(dep_file, provide_to_file_hash, file_to_require_hash, closure_path):
+def process_deps(dep_file, provide_to_file_hash, file_to_require_hash):
   for line in open(dep_file,"r").readlines():
-    process_line(line, provide_to_file_hash, file_to_require_hash, closure_path)
+    process_line(line, provide_to_file_hash, file_to_require_hash)
 
 def populate_files(js_file, files_array, provide_to_file_hash, file_to_require_hash):
   if(not js_file in files_array):
@@ -198,13 +193,13 @@ def populate_files(js_file, files_array, provide_to_file_hash, file_to_require_h
       next_file = provide_to_file_hash[symbol]
       populate_files(next_file, files_array, provide_to_file_hash, file_to_require_hash)
 
-def print_help_core(jar_path):
-  command = get_closure_base(jar_path)
-  command.append("--help")
-  return command
-
-def print_tree_core(jar_path, closure_path, js_files, extern_files):
-  command = get_command_with_inputs(jar_path, closure_path, js_files, extern_files)
-  command.append("--print_tree")
-  command.append("true")
-  return command
+# def print_help():
+#   command = get_closure_base()
+#   command.append("--help")
+#   return command, None, None
+# 
+# def print_tree_core(js_files, extern_files):
+#   command = get_command_with_inputs(js_files, extern_files)
+#   command.append("--print_tree")
+#   command.append("true")
+#   return command, None, None
--- build.py
+++ build.py
@@ -7,22 +7,10 @@ js_path = "javascripts"
 closure_path = os.path.join(js_path, 'closure-library','closure')
 application_js_path = os.path.join(js_path, 'application.js')
 js_dirs = map(lambda dir: os.path.join(js_path, dir), ['box2d','eightball','helpers'])
-closure_dependencies = js_dirs + [application_js_path]
-
-# deps
-calcdeps_py_path = os.path.join(closure_path, "bin", "calcdeps.py")
-deps_js_path = os.path.join(js_path, "deps.js")
-
-# compile
-compiled_js_path = os.path.join(js_path, "compiled.js")
-jar_path = os.path.join('_tools', 'closure_compiler', 'compiler.jar')
-extern_dir = os.path.join(js_path, 'externs')
 
 closure = Closure(
-  closure_path = closure_path,
   application_js_path = application_js_path,
   closure_dependencies = js_dirs + [application_js_path],
-  calcdeps_py_path = calcdeps_py_path,
   deps_js_path = os.path.join(js_path, "deps.js"),
   compiled_js_path = os.path.join(js_path, "compiled.js"),
   extern_dir = os.path.join(js_path, 'externs')
