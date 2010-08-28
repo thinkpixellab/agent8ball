539da5b9276331d036afec3e078ca206a88a8464
ingebretsen <ingebretsen@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-28 16:05:14 +0000

    Added the preloader and new start graphis.
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@260 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- javascripts/application.js
+++ javascripts/application.js
@@ -5,7 +5,6 @@ goog.require('goog.events.EventType');
 goog.require('goog.debug.LogManager');
 goog.require('goog.i18n.NumberFormat');
 
-goog.require('pixelLab.ImagePreloader');
 goog.require('pixelLab.DebugDiv');
 
 goog.require('eightball.PoolTable');
@@ -18,20 +17,44 @@ goog.require('eightball.Game.GameState');
 
 var _game;
 
-// displays loading information and preloads all other content then
-// calls loadApp
-var loadContent = function() {
-  pixelLab.DebugDiv.enable();
-  pixelLab.ImagePreloader.preload("images/bestscore.png, images/cue.png, images/progressbg.png, images/progressunit.png, images/score.png, images/table.jpg, images/tableborder.png, images/timeremaining.png, images/wood.jpg");
-  goog.global.setTimeout(loadApp, 500);
+
+
+// *******************************************************************************
+// PRELOADER
+// *******************************************************************************
+
+var files = ['images/ballvignette.png', 'images/bestscore.png', 'images/cue.png', 'images/howtoplay.png', 'images/instructions.png', 'images/lightoff.png', 'images/lighton.png', 'images/loading.jpg', 'images/loadingbg.png', 'images/loadingpg.png', 'images/num1.png', 'images/num10.png', 'images/num11.png', 'images/num12.png', 'images/num13.png', 'images/num14.png', 'images/num15.png', 'images/num2.png', 'images/num3.png', 'images/num4.png', 'images/num5.png', 'images/num6.png', 'images/num7.png', 'images/num8.png', 'images/num9.png', 'images/pause.png', 'images/progressbg.png', 'images/progressunit.png', 'images/score.png', 'images/smalllogo.png', 'images/speaker.png', 'images/startover.png', 'images/table.jpg', 'images/tableborder.png', 'images/timeremaining.png', 'images/transparent.png', 'images/triangle.png', 'images/vignette.png', 'images/wood.jpg', 'sounds/shot00.mp3', 'sounds/shot01.mp3', 'sounds/shot02.mp3', 'sounds/shot03.mp3', 'sounds/shot04.mp3', 'sounds/shot05.mp3', 'sounds/theme.mp4'];
+
+var prog = function (perc) {
+  $('#loadingpg').width(237 * perc);
+}
+
+var done = function () {
+    $("#loadingbg").delay(500).fadeOut(700, loadApp);
+}
+
+var loadContent = function () {
+
+  // show the preload ui
+  $('#loadingbg').delay(500).fadeIn(700);
+
+  // load the ui (on a timer so that we start after fading in -- it looks weird otherwise
+  setTimeout(function () { preload(files, prog, done); }, 1500);
 };
 
-var loadApp = function() {
+
+// *******************************************************************************
+// APP LOGIC
+// *******************************************************************************
+
+var loadApp = function () {
 
   // show the content, hide the loading element
-  $('#loading').fadeOut(400);
+  $('#vignette').fadeIn(1000);
   $('#game').fadeIn(1000);
   $('#gamecontrolsouter').fadeIn(1000);
+  $('#cue_canvas').fadeIn(1000);
+  $('#overlay').fadeIn(1000);
 
   // music
   var musicManager = new eightball.Music("sounds/theme.mp4");
@@ -95,6 +118,7 @@ var loadApp = function() {
   };
 
   var _readyAction = function() {
+    overlay.fadeIn(1000 );
     start.delay(800).fadeIn(400);
   };
 
@@ -121,7 +145,6 @@ var loadApp = function() {
   goog.events.listen(game, eightball.Game.EventType.HIGHSCORE, _highScoreAction);
   goog.events.listen(game, eightball.Game.EventType.READY, _readyAction);
 
-
   // register for collision events (for sounds)
   goog.events.listen(poolTable, eightball.PoolTable.EventType.WALL_HIT, function () {
     //goog.debug.LogManager.getRoot().info("Wall hit!");
@@ -134,7 +157,6 @@ var loadApp = function() {
     soundManager.play("ball" + Math.floor(Math.random() * 6));
   },
   undefined, this);
-    
   
   // calling reset after the game has been loaded fires the events we 
   // need to initialize everything for game play
@@ -196,8 +218,7 @@ var loadApp = function() {
 
   // sound effects test code
   $(".soundtest").click(function() {
-    soundManager.play(this.id);
-  });
+    soundManager.play(this.id); });
 };
 
-$(window).load(loadContent);
+$(document).ready(loadContent);
---
+++ javascripts/preload.js
@@ -0,0 +1,78 @@
+﻿$.ajaxSetup({ cache: true, async: true, timeout: 1200000, type: "GET",  });
+
+// globals for tracking progress
+var itemsTotal = 0;
+var itemsLoaded = 0;
+var progress = null;
+var complete = null;
+
+var preload = function (urls, progressCallback, completedCallback) {
+  itemsTotal = urls.length;
+  complete = completedCallback;
+  progress = progressCallback;
+  
+  for (i = 0; i < urls.length; i++) {
+    downloadFile(urls[i]);
+  }
+};
+
+// download an individual file
+var downloadFile = function (url) {
+  var g = function() {
+     $.get(url, function (data) { incrementDownloadCount() })
+  };
+  $.async.add(g);
+};
+
+// even though there was an error, we want to update our count
+$(document).ajaxError(function (event, request, settings) {
+  incrementDownloadCount();
+});
+
+// update our counts and call the appropriate events
+var incrementDownloadCount = function () {
+  itemsLoaded++;
+  percentComplete = itemsLoaded / itemsTotal;
+
+  if (progress != null) {
+    progress.call(this, percentComplete);    
+  }
+  if (percentComplete == 1.0) {
+    complete.call();
+  }
+};
+
+// keeps our ui from freezing while we hammer the browser with ajax
+$.async = {
+    _timer: null,
+    _queue: [],
+    add: function(fn, context, time) {
+        var setTimer = function(time) {
+            $.queue._timer = setTimeout(function() {
+                time = $.async.add();
+                if ($.async._queue.length) {
+                    setTimer(time);
+                }
+            }, time || 2);
+        }
+
+        if (fn) {
+            $.async._queue.push([fn, context, time]);
+            if ($.async._queue.length == 1) {
+                setTimer(time);
+            }
+            return;
+        }
+
+        var next = $.async._queue.shift();
+        if (!next) {
+            return 0;
+        }
+        next[0].call(next[1] || window);
+        return next[2];
+    },
+    clear: function() {
+        clearTimeout($.queue._timer);
+        $.async._queue = [];
+    }
+};
