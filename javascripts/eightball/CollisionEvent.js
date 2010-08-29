8d7d26917c568416b26708129dbf8280b02ee050
ingebretsen <ingebretsen@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-29 19:08:59 +0000

    missing file from last checkin
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@269 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

---
+++ javascripts/eightball/CollisionEvent.js
@@ -0,0 +1,44 @@
+﻿//
+goog.provide('eightball.CollisionEvent');
+
+goog.require('goog.events.Event');
+
+/**
+* Object representing a collision between balls or a ball and a wall
+*
+* @extends {goog.events.Event}
+* @constructor
+* @param {number} avgVelocity
+* @param {number} ballCount
+* @param {number} wallCount
+* @param {Object=} opt_target Reference to the object that is the target of
+*     this event. It has to implement the {@code EventTarget} interface
+*     declared at {@link http://developer.mozilla.org/en/DOM/EventTarget}.
+*/
+eightball.CollisionEvent = function (avgVelocity, ballCount, wallCount, opt_target) {
+
+  goog.events.Event.call(this, eightball.CollisionEvent.TYPE, opt_target);
+
+  /**
+  @type {number}
+  */
+  this.avgVelocity = avgVelocity;
+
+  /**
+  @type {number}
+  */
+  this.ballCount = ballCount;
+
+  /**
+  @type {number}
+  */
+  this.wallCount = wallCount;
+
+};
+goog.inherits(eightball.CollisionEvent, goog.events.Event);
+
+/**
+@const
+@type {string}
+*/
+eightball.CollisionEvent.TYPE = 'eightball.CollisionEvent.TYPE';
