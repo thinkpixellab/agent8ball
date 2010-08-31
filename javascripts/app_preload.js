39a0aa6dec7ab9b0ad91c99aa891d1052e4f685a
kevin <kevin@efd6ee18-2b61-4776-8ca0-166b4ef03a6b>
2010-08-31 18:35:33 +0000

    Added SKIP_PRELOAD
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@280 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- javascripts/app_preload.js
+++ javascripts/app_preload.js
@@ -1,24 +1,31 @@
-﻿
+﻿// Handles pre-loading the app
 goog.require('pixelLab.Preload');
 
+/** @define {boolean} */
+var SKIP_PRELOAD = false;
+
 $(document).ready(function() {
+  if (SKIP_PRELOAD) {
+    loadApp();
+  } else {
 
-  var files = ['images/ballvignette.png', 'images/bestscore.png', 'images/cue.png', 'images/howtoplay.png', 'images/instructions.png', 'images/lightoff.png', 'images/lighton.png', 'images/loading.jpg', 'images/loadingbg.png', 'images/loadingpg.png', 'images/num1.png', 'images/num10.png', 'images/num11.png', 'images/num12.png', 'images/num13.png', 'images/num14.png', 'images/num15.png', 'images/num2.png', 'images/num3.png', 'images/num4.png', 'images/num5.png', 'images/num6.png', 'images/num7.png', 'images/num8.png', 'images/num9.png', 'images/pause.png', 'images/progressbg.png', 'images/progressunit.png', 'images/score.png', 'images/smalllogo.png', 'images/speaker.png', 'images/startover.png', 'images/table.jpg', 'images/tableborder.png', 'images/timeremaining.png', 'images/transparent.png', 'images/triangle.png', 'images/vignette.png', 'images/wood.jpg', 'sounds/shot00.mp3', 'sounds/shot01.mp3', 'sounds/shot02.mp3', 'sounds/shot03.mp3', 'sounds/shot04.mp3', 'sounds/shot05.mp3', 'sounds/theme.mp4'];
+    var files = ['images/ballvignette.png', 'images/bestscore.png', 'images/cue.png', 'images/howtoplay.png', 'images/instructions.png', 'images/lightoff.png', 'images/lighton.png', 'images/loading.jpg', 'images/loadingbg.png', 'images/loadingpg.png', 'images/num1.png', 'images/num10.png', 'images/num11.png', 'images/num12.png', 'images/num13.png', 'images/num14.png', 'images/num15.png', 'images/num2.png', 'images/num3.png', 'images/num4.png', 'images/num5.png', 'images/num6.png', 'images/num7.png', 'images/num8.png', 'images/num9.png', 'images/pause.png', 'images/progressbg.png', 'images/progressunit.png', 'images/score.png', 'images/smalllogo.png', 'images/speaker.png', 'images/startover.png', 'images/table.jpg', 'images/tableborder.png', 'images/timeremaining.png', 'images/transparent.png', 'images/triangle.png', 'images/vignette.png', 'images/wood.jpg', 'sounds/shot00.mp3', 'sounds/shot01.mp3', 'sounds/shot02.mp3', 'sounds/shot03.mp3', 'sounds/shot04.mp3', 'sounds/shot05.mp3', 'sounds/theme.mp4'];
 
-  var prog = function(perc) {
-    $('#loadingpg').width(237 * perc);
-  };
+    var prog = function(perc) {
+      $('#loadingpg').width(237 * perc);
+    };
 
-  var done = function() {
-    $("#loadingbg").delay(500).fadeOut(700, loadApp);
-  };
+    var done = function() {
+      $("#loadingbg").delay(500).fadeOut(700, loadApp);
+    };
 
-  // show the preload ui
-  $('#loadingbg').delay(500).fadeIn(700);
+    // show the preload ui
+    $('#loadingbg').delay(500).fadeIn(700);
 
-  // load the ui (on a timer so that we start after fading in -- it looks weird otherwise
-  setTimeout(function() {
-    new pixelLab.Preload(files, prog, done);
-  },
-  1500);
+    // load the ui (on a timer so that we start after fading in -- it looks weird otherwise
+    setTimeout(function() {
+      new pixelLab.Preload(files, prog, done);
+    },
+    1500);
+  }
 });
