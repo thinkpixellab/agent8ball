0e1e649fadccf76590869bfb40df8b4224f8ea9a
Kevin Moore <kevin@thinkpixellab.com>
2010-09-14 05:54:40 +0000

    Clean-up to b2Island
    git-svn-id: https://pixellab.springloops.com/source/eightball/trunk@403 efd6ee18-2b61-4776-8ca0-166b4ef03a6b

--- javascripts/box2d/dynamics/b2Island.js
+++ javascripts/box2d/dynamics/b2Island.js
@@ -130,171 +130,147 @@ b2Island = function(bodyCapacity, contactCapacity, jointCapacity, allocator) {
   this.m_allocator = allocator;
 };
 
-b2Island.prototype = {
-  //~b2Island();
-  Clear: function() {
-    this.m_bodyCount = 0;
-    this.m_contactCount = 0;
-    this.m_jointCount = 0;
-  },
+b2Island.prototype.Clear = function() {
+  this.m_bodyCount = 0;
+  this.m_contactCount = 0;
+  this.m_jointCount = 0;
+};
+b2Island.prototype.Solve = function(step, gravity) {
+  var i = 0;
+  var b;
 
-  Solve: function(step, gravity) {
-    var i = 0;
-    var b;
+  for (i = 0; i < this.m_bodyCount; ++i) {
+    b = this.m_bodies[i];
 
+    if (b.m_invMass == 0.0) continue;
+
+    b.m_linearVelocity.Add(b2Math.MulFV(step.dt, b2Math.AddVV(gravity, b2Math.MulFV(b.m_invMass, b.m_force))));
+    b.m_angularVelocity += step.dt * b.m_invI * b.m_torque;
+
+    //b.m_linearVelocity *= b.m_linearDamping;
+    b.m_linearVelocity.Multiply(b.m_linearDamping);
+    b.m_angularVelocity *= b.m_angularDamping;
+
+    // Store positions for conservative advancement.
+    b.m_position0.SetV(b.m_position);
+    b.m_rotation0 = b.m_rotation;
+  }
+
+  var contactSolver = new b2ContactSolver(this.m_contacts, this.m_contactCount, this.m_allocator);
+
+  // Pre-solve
+  contactSolver.PreSolve();
+
+  for (i = 0; i < this.m_jointCount; ++i) {
+    this.m_joints[i].PrepareVelocitySolver();
+  }
+
+  // this.Solve velocity constraints.
+  for (i = 0; i < step.iterations; ++i) {
+    contactSolver.SolveVelocityConstraints();
+
+    for (var j = 0; j < this.m_jointCount; ++j) {
+      this.m_joints[j].SolveVelocityConstraints(step);
+    }
+  }
+
+  // Integrate positions.
+  for (i = 0; i < this.m_bodyCount; ++i) {
+    b = this.m_bodies[i];
+
+    if (b.m_invMass == 0.0) continue;
+
+    //b.m_position.Add( b2Math.MulFV (step.dt, b.m_linearVelocity) );
+    b.m_position.x += step.dt * b.m_linearVelocity.x;
+    b.m_position.y += step.dt * b.m_linearVelocity.y;
+    b.m_rotation += step.dt * b.m_angularVelocity;
+
+    b.m_R.Set(b.m_rotation);
+  }
+
+  for (i = 0; i < this.m_jointCount; ++i) {
+    this.m_joints[i].PreparePositionSolver();
+  }
+
+  // this.Solve position constraints.
+  if (b2World.s_enablePositionCorrection) {
+    for (b2Island.m_positionIterationCount = 0; b2Island.m_positionIterationCount < step.iterations; ++b2Island.m_positionIterationCount) {
+      var contactsOkay = contactSolver.SolvePositionConstraints(b2Settings.b2_contactBaumgarte);
+
+      var jointsOkay = true;
+      for (i = 0; i < this.m_jointCount; ++i) {
+        var jointOkay = this.m_joints[i].SolvePositionConstraints();
+        jointsOkay = jointsOkay && jointOkay;
+      }
+
+      if (contactsOkay && jointsOkay) {
+        break;
+      }
+    }
+  }
+
+  // Post-solve.
+  contactSolver.PostSolve();
+
+  // Synchronize shapes and reset forces.
+  for (i = 0; i < this.m_bodyCount; ++i) {
+    b = this.m_bodies[i];
+
+    if (b.m_invMass == 0.0) continue;
+
+    b.m_R.Set(b.m_rotation);
+
+    b.SynchronizeShapes();
+    b.m_force.Set(0.0, 0.0);
+    b.m_torque = 0.0;
+  }
+};
+b2Island.prototype.UpdateSleep = function(dt) {
+  var i = 0;
+  var b;
+
+  var minSleepTime = Number.MAX_VALUE;
+
+  var linTolSqr = b2Settings.b2_linearSleepTolerance * b2Settings.b2_linearSleepTolerance;
+  var angTolSqr = b2Settings.b2_angularSleepTolerance * b2Settings.b2_angularSleepTolerance;
+
+  for (i = 0; i < this.m_bodyCount; ++i) {
+    b = this.m_bodies[i];
+    if (b.m_invMass == 0.0) {
+      continue;
+    }
+
+    if ((b.m_flags & b2Body.e_allowSleepFlag) == 0) {
+      b.m_sleepTime = 0.0;
+      minSleepTime = 0.0;
+    }
+
+    if ((b.m_flags & b2Body.e_allowSleepFlag) == 0 || b.m_angularVelocity * b.m_angularVelocity > angTolSqr || b2Math.b2Dot(b.m_linearVelocity, b.m_linearVelocity) > linTolSqr) {
+      b.m_sleepTime = 0.0;
+      minSleepTime = 0.0;
+    } else {
+      b.m_sleepTime += dt;
+      minSleepTime = b2Math.b2Min(minSleepTime, b.m_sleepTime);
+    }
+  }
+
+  if (minSleepTime >= b2Settings.b2_timeToSleep) {
     for (i = 0; i < this.m_bodyCount; ++i) {
       b = this.m_bodies[i];
-
-      if (b.m_invMass == 0.0) continue;
-
-      b.m_linearVelocity.Add(b2Math.MulFV(step.dt, b2Math.AddVV(gravity, b2Math.MulFV(b.m_invMass, b.m_force))));
-      b.m_angularVelocity += step.dt * b.m_invI * b.m_torque;
-
-      //b.m_linearVelocity *= b.m_linearDamping;
-      b.m_linearVelocity.Multiply(b.m_linearDamping);
-      b.m_angularVelocity *= b.m_angularDamping;
-
-      // Store positions for conservative advancement.
-      b.m_position0.SetV(b.m_position);
-      b.m_rotation0 = b.m_rotation;
+      b.m_flags |= b2Body.e_sleepFlag;
     }
-
-    var contactSolver = new b2ContactSolver(this.m_contacts, this.m_contactCount, this.m_allocator);
-
-    // Pre-solve
-    contactSolver.PreSolve();
-
-    for (i = 0; i < this.m_jointCount; ++i) {
-      this.m_joints[i].PrepareVelocitySolver();
-    }
-
-    // this.Solve velocity constraints.
-    for (i = 0; i < step.iterations; ++i) {
-      contactSolver.SolveVelocityConstraints();
-
-      for (var j = 0; j < this.m_jointCount; ++j) {
-        this.m_joints[j].SolveVelocityConstraints(step);
-      }
-    }
-
-    // Integrate positions.
-    for (i = 0; i < this.m_bodyCount; ++i) {
-      b = this.m_bodies[i];
-
-      if (b.m_invMass == 0.0) continue;
-
-      //b.m_position.Add( b2Math.MulFV (step.dt, b.m_linearVelocity) );
-      b.m_position.x += step.dt * b.m_linearVelocity.x;
-      b.m_position.y += step.dt * b.m_linearVelocity.y;
-      b.m_rotation += step.dt * b.m_angularVelocity;
-
-      b.m_R.Set(b.m_rotation);
-    }
-
-    for (i = 0; i < this.m_jointCount; ++i) {
-      this.m_joints[i].PreparePositionSolver();
-    }
-
-    // this.Solve position constraints.
-    if (b2World.s_enablePositionCorrection) {
-      for (b2Island.m_positionIterationCount = 0; b2Island.m_positionIterationCount < step.iterations; ++b2Island.m_positionIterationCount) {
-        var contactsOkay = contactSolver.SolvePositionConstraints(b2Settings.b2_contactBaumgarte);
-
-        var jointsOkay = true;
-        for (i = 0; i < this.m_jointCount; ++i) {
-          var jointOkay = this.m_joints[i].SolvePositionConstraints();
-          jointsOkay = jointsOkay && jointOkay;
-        }
-
-        if (contactsOkay && jointsOkay) {
-          break;
-        }
-      }
-    }
-
-    // Post-solve.
-    contactSolver.PostSolve();
-
-    // Synchronize shapes and reset forces.
-    for (i = 0; i < this.m_bodyCount; ++i) {
-      b = this.m_bodies[i];
-
-      if (b.m_invMass == 0.0) continue;
-
-      b.m_R.Set(b.m_rotation);
-
-      b.SynchronizeShapes();
-      b.m_force.Set(0.0, 0.0);
-      b.m_torque = 0.0;
-    }
-  },
-
-  UpdateSleep: function(dt) {
-    var i = 0;
-    var b;
-
-    var minSleepTime = Number.MAX_VALUE;
-
-    var linTolSqr = b2Settings.b2_linearSleepTolerance * b2Settings.b2_linearSleepTolerance;
-    var angTolSqr = b2Settings.b2_angularSleepTolerance * b2Settings.b2_angularSleepTolerance;
-
-    for (i = 0; i < this.m_bodyCount; ++i) {
-      b = this.m_bodies[i];
-      if (b.m_invMass == 0.0) {
-        continue;
-      }
-
-      if ((b.m_flags & b2Body.e_allowSleepFlag) == 0) {
-        b.m_sleepTime = 0.0;
-        minSleepTime = 0.0;
-      }
-
-      if ((b.m_flags & b2Body.e_allowSleepFlag) == 0 || b.m_angularVelocity * b.m_angularVelocity > angTolSqr || b2Math.b2Dot(b.m_linearVelocity, b.m_linearVelocity) > linTolSqr) {
-        b.m_sleepTime = 0.0;
-        minSleepTime = 0.0;
-      } else {
-        b.m_sleepTime += dt;
-        minSleepTime = b2Math.b2Min(minSleepTime, b.m_sleepTime);
-      }
-    }
-
-    if (minSleepTime >= b2Settings.b2_timeToSleep) {
-      for (i = 0; i < this.m_bodyCount; ++i) {
-        b = this.m_bodies[i];
-        b.m_flags |= b2Body.e_sleepFlag;
-      }
-    }
-  },
-
-  AddBody: function(body) {
-    //b2Settings.b2Assert(this.m_bodyCount < this.m_bodyCapacity);
-    this.m_bodies[this.m_bodyCount++] = body;
-  },
-
-  AddContact: function(contact) {
-    //b2Settings.b2Assert(this.m_contactCount < this.m_contactCapacity);
-    this.m_contacts[this.m_contactCount++] = contact;
-  },
-
-  AddJoint: function(joint) {
-    //b2Settings.b2Assert(this.m_jointCount < this.m_jointCapacity);
-    this.m_joints[this.m_jointCount++] = joint;
-  },
-
-  m_allocator: null,
-
-  m_bodies: null,
-  m_contacts: null,
-  m_joints: null,
-
-  m_bodyCount: 0,
-  m_jointCount: 0,
-  m_contactCount: 0,
-
-  m_bodyCapacity: 0,
-  m_contactCapacity: 0,
-  m_jointCapacity: 0,
-
-  m_positionError: null
+  }
+};
+b2Island.prototype.AddBody = function(body) {
+  //b2Settings.b2Assert(this.m_bodyCount < this.m_bodyCapacity);
+  this.m_bodies[this.m_bodyCount++] = body;
+};
+b2Island.prototype.AddContact = function(contact) {
+  //b2Settings.b2Assert(this.m_contactCount < this.m_contactCapacity);
+  this.m_contacts[this.m_contactCount++] = contact;
+};
+b2Island.prototype.AddJoint = function(joint) {
+  //b2Settings.b2Assert(this.m_jointCount < this.m_jointCapacity);
+  this.m_joints[this.m_jointCount++] = joint;
 };
 b2Island.m_positionIterationCount = 0;
