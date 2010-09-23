76e0b40a2d11e4c0d95d1834abe1f446c9db68c4
Kevin Moore <kevin@thinkpixellab.com>
2010-09-23 09:38:29 -0700

    Moving helper code to main_helper

---
+++ app/helpers/main_helper.rb
@@ -0,0 +1,12 @@
+module MainHelper
+  def game_js(compiled = false)
+    items = [javascript_include_tag('/assets.js')]
+    if compiled
+      items << javascript_include_tag('compiled')
+    else
+      items << content_tag('script', '', {'type' => Mime::JS, 'src' => '/javascripts/closure-library/closure/goog/base.js'})
+      items << javascript_include_tag('deps', 'loader')
+    end
+    items.join("\n").html_safe
+  end
+end
