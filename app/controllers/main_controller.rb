9c517ec522b75a25efe003c0c9a308ff70a61d2f
Kevin Moore <kevin@thinkpixellab.com>
2010-10-11 10:50:32 -0700

    Making main view cache-able

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
+  def index
+    # As long as this view has no state, should be cache-able
+    fresh_when(:public => true)
+  end
+
   def handle404
     render :status => 404
   end
