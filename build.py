8f5d411e46ace650bfed64e4e013efafcc53080d
Kevin Moore <kevin@thinkpixellab.com>
2010-10-17 17:50:34 -0700

    Everything in externs is an extern - glob is your friend

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import glob
 from _tools.Closure import Closure
 import _tools.HtmlPost
 from _tools.HtmlCompressor import HtmlCompressor, CssCompressor
@@ -10,8 +11,8 @@ js_path = os.path.join('public', 'javascripts')
 
 closure_path = os.path.join(js_path, 'closure-library','closure')
 
-app_extern = os.path.join(js_path, 'externs', 'application_extern.js')
-jquery_extern = os.path.join(js_path, 'externs', 'jquery_extern.js')
+externs = os.path.join(js_path, 'externs', '*.js')
+externs = glob.glob(externs)
 
 loader_js_path = os.path.join(js_path, 'loader.js')
 deps_path = os.path.join(js_path, 'deps.js')
@@ -27,5 +28,5 @@ Closure(
   closure_dependencies = js_dirs + [loader_js_path, application_js_path],
   deps_js_path = deps_path,
   compiled_js_path = compiled_path,
-  extern_files = [app_extern, jquery_extern],
+  extern_files = externs,
 ).deps_and_compile(debug = False)
