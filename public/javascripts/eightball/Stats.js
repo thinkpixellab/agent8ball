ef10dc2ac36775bb0958c4f6e5a37238c4239047
Kevin Moore <kevin@thinkpixellab.com>
2010-10-20 12:14:31 -0700

    Huge oops...wrong google analytics code

--- public/javascripts/eightball/Stats.js
+++ public/javascripts/eightball/Stats.js
@@ -4,7 +4,7 @@ goog.require('pixelLab.Stats');
 
 eightball.Stats.load = function() {
   if (window['doStats']) {
-    pixelLab.Stats.addGoogleAnalytics('UA-18601140-1');
+    pixelLab.Stats.addGoogleAnalytics('UA-18513960-1');
     pixelLab.Stats.addStatCounter(6221324, '0296de03');
   }
 };
