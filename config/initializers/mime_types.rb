400ddc47ff6725e333c0d50e56241cd186c7ffef
Kevin Moore <kevin@thinkpixellab.com>
2010-10-21 22:12:29 -0700

    Adding audio mime types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# Mime::Type.register "text/html", :html, %w( application/xhtml+xml ), %w( xhtml )
+Mime::Type.register 'video/webm', :webm
+Mime::Type.register 'audio/mpeg', :mp3
+Mime::Type.register 'audio/mp4', :mp4
+
