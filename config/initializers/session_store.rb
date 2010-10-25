8336be2d216e872c363bdc8a372dd2418f96ae60
Kevin Moore <kevin@thinkpixellab.com>
2010-10-24 22:42:51 -0700

    rails session cookie: not used, not set

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Agent8ballRails::Application.config.session_store :cookie_store, :key => '_agent8ball_rails_session'
+Agent8ballRails::Application.config.session_store :disabled
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
