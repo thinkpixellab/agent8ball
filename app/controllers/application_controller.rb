c9107c03c04dddaf6110b63073ed7beef17ef28e
Kevin Moore <kevin@thinkpixellab.com>
2010-10-26 18:49:51 -0700

    Redirect every sub domain (except beta) to agent8ball.com
    Closes #20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :right_domain
+
+  private
+    def right_domain
+      if request.subdomain.present? && !%(beta).include?(request.subdomain)
+        redirect_to 'http://agent8ball.com', :status => :moved_permanently
+      end
+    end
 end
