da5f0f45c901fe90d0609d000aef0cbad6f5a5c9
Kevin Moore <kevin@d-69-91-180-170.dhcp4.washington.edu>
2011-05-11 20:45:58 -0700

    Removed unneeded compass init

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,14 +1,10 @@
 require 'fileutils'
-require 'compass'
-require 'compass/app_integration/rails'
 
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
     :urls => ['/stylesheets'],
     :root => "#{Rails.root}/tmp")
-
-Compass::AppIntegration::Rails.initialize!
 
 module ActionView
   module Helpers
